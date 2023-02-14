bugfix for brute-force statistic calculation

src/fairness-audit/auditor.py
@@ -24,7 +24,7 @@ class Auditor:
         ----------
         X : covariates (n, k)
         Y : outcomes (n, 1)
-        Z : predictions (n, 1)
+        Z : predictions (n, p)
         metric : Metric
         """
         assert X.shape[0] == Y.shape[0] == Z.shape[0]
@@ -53,7 +53,7 @@ class Auditor:
         type : str,
         groups : Union[np.ndarray, str],
         epsilon : float = None,
-        bootstrap_params : dict = None
+        bootstrap_params : dict = {}
     ) -> None:
         """
         Obtain bootstrap critical values for a specific group collection
@@ -67,8 +67,10 @@ class Auditor:
         bootstrap_params : dict = None
         """
         if isinstance(groups, str):
+            if epsilon == None:
+                raise ValueError(f"Only fixed-epsilon certification supported with {groups}.")
             groups_name = groups
-            group_dummies = np.ones((len(self.L), 1))
+            group_dummies = np.ones((len(self.L), 1), dtype=bool)
         else:
             groups_name = "exhaustive"
             group_dummies = groups
@@ -85,8 +87,9 @@ class Auditor:
 
         self.type = type
         self.epsilon = epsilon
+        self.critical_values = []
 
-        for g_dummies in groups_list:
+        for g_dummies in self.groups_list:
             all_dummies = np.amax(g_dummies, axis=1)
             threshold = self.metric.compute_threshold(
                 self.Z[all_dummies], 
@@ -126,18 +129,24 @@ class Auditor:
 
     def query_group(
         self,
-        group : int
+        group : Union[np.ndarray, int]
     ) -> Tuple[float, bool]:
         if self.critical_values is None:
             raise ValueError("Run calibrate before querying for groups.")
 
         results = []
+        metric_values = []
         for g_dummies, critical_value in zip(self.groups_list, self.critical_values):
-            dummies = g_dummies[:,group]
+            if isinstance(group, int):
+                dummies = g_dummies[:,group]
+            else:
+                all_dummies = np.amax(g_dummies, axis=1)
+                dummies = group * all_dummies
             metric_value = np.sum(self.L * dummies) / dummies.sum()
+            metric_values.append(metric_value)
             results.append(self._certify_group(metric_value, dummies, g_dummies, critical_value))
 
-        return results
+        return results, metric_values
 
     def calibrate_rkhs(
         self,
@@ -146,7 +155,7 @@ class Auditor:
         kernel_params : dict = {},
         bootstrap_params : dict = {}
     ) -> None:
-        vacuous_group = np.ones((self.X.shape[0], 1))
+        vacuous_group = np.ones((len(self.L), 1), dtype=bool)
         if self.metric.requires_conditioning():
             self.groups_list = self.metric.get_conditional_groups(
                 vacuous_group,
@@ -171,7 +180,7 @@ class Auditor:
             K_sqrt = np.linalg.cholesky(K)
 
             self.critical_values.append(
-                estimate_critical_value("RKHS", alpha, bootstrap_params, dict(K_sqrt=K_sqrt))
+                estimate_critical_value("RKHS", alpha, self.L[group_dummies], bootstrap_params, dict(K_sqrt=K_sqrt))
             )
 
     def query_rkhs(
@@ -215,9 +224,9 @@ class Auditor:
     ) -> Union[bool, float]:
         all_dummies = np.amax(group_dummies, axis=1)
         threshold = self.metric.compute_threshold(self.Z[all_dummies], self.Y[all_dummies])
-        group_prob = np.sum(dummies) / len(dummies)
+        group_prob = np.mean(dummies)
 
-        if self.epsilon:
+        if self.epsilon != None:
             if self.type == "lower":
                 return metric_value < threshold + self.epsilon + critical_value / group_prob
             elif self.type == "upper":

src/fairness-audit/bootstrap.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.stats
 
-from groups import Groups, score_intervals
+from groups import score_intervals
 from shifts import score_rkhs
 from metrics import Metric
 
@@ -12,8 +12,8 @@ BOOTSTRAP_DEFAULTS = {
     "B": 500,
     "method": "multinomial",
     "student": None,
-    "student_threshold": "adaptive",
-    "seed": 0
+    "student_threshold": 0.05**(3/2),
+    "seed": 1
 }
 
 def _compute_bound_statistic(
@@ -25,12 +25,12 @@ def _compute_bound_statistic(
     group_dummies : np.ndarray,
     w : np.ndarray
 ) -> Tuple[np.ndarray, np.ndarray]:
-    Y_b = np.repeat(Y, w, axis=0)
-    Z_b = np.repeat(Z, w, axis=0)
+    Y_b = np.repeat(Y, w.flatten(), axis=0)
+    Z_b = np.repeat(Z, w.flatten(), axis=0)
     threshold_b = metric.compute_threshold(Z_b, Y_b)
 
-    L = L - threshold
-    L_b = w * (L - threshold_b)
+    L = (L - threshold).reshape(-1,1)
+    L_b = (w * (L - threshold_b)).reshape(-1,1)
 
     # form group statistics
     # (L, L_b, 1, w)
@@ -58,17 +58,17 @@ def _compute_fixed_statistic(
     w : np.ndarray,
     epsilon : float
 ) -> Tuple[np.ndarray, np.ndarray]:
-    Y_b = np.repeat(Y, w, axis=0)
-    Z_b = np.repeat(Z, w, axis=0)
+    Y_b = np.repeat(Y, w.flatten(), axis=0)
+    Z_b = np.repeat(Z, w.flatten(), axis=0)
     threshold_b = metric.compute_threshold(Z_b, Y_b)
 
-    L = L - threshold - epsilon
-    L_b = w * (L - threshold_b - epsilon)
+    L_n = (L - threshold - epsilon).reshape(-1,1)
+    L_b = (w.flatten() * (L - threshold_b - epsilon).flatten()).reshape(-1,1)
 
     # form group statistics
     # (L, L_b, 1)
     mat = np.concatenate(
-        (L, L_b, np.ones_like(w)),
+        (L_n, L_b, np.ones_like(w)),
         axis=1
     )
 
@@ -79,26 +79,29 @@ def _compute_fixed_statistic(
 
     stats = group_mat[:,1] - group_mat[:,0]
 
-    return stats.flatten(), group_mat
+    return stats, group_mat
 
 def estimate_bootstrap_distribution(
     Y : np.ndarray,
     Z : np.ndarray,
     L : np.ndarray,
     threshold : float,
-    groups : Groups,
+    group_dummies : np.ndarray,
     metric : Metric,
     epsilon : float = None,
     bootstrap_params : dict = {}
 ) -> np.ndarray:
     n = Y.shape[0]
-    n_groups = groups.dummies.shape[1]
+    n_groups = group_dummies.shape[1]
 
     B = bootstrap_params.get("B", BOOTSTRAP_DEFAULTS["B"])
     method = bootstrap_params.get("method", BOOTSTRAP_DEFAULTS["method"])
 
-    b_statistics = np.empty((B, n_groups))
-    group_statistics = np.empty((B, n_groups, 4))
+    b_statistics = np.zeros((B, n_groups))
+    if epsilon is None:
+        group_statistics = np.empty((B, n_groups, 4))
+    else:
+        group_statistics = np.empty((B, n_groups, 3))
     
     rng = np.random.default_rng(
         seed=bootstrap_params.get("seed", BOOTSTRAP_DEFAULTS["seed"])
@@ -117,7 +120,7 @@ def estimate_bootstrap_distribution(
                 L,
                 threshold,
                 metric,
-                groups.dummies,
+                group_dummies,
                 w
             )
         else:
@@ -127,7 +130,7 @@ def estimate_bootstrap_distribution(
                 L,
                 threshold,
                 metric,
-                groups.dummies,
+                group_dummies,
                 w, 
                 epsilon
             )
@@ -176,17 +179,17 @@ def estimate_critical_value(
     scores = []
     for b in tqdm(range(B)):
         if method == 'multinomial':
-            w = rng.multinomial(n, [1/n] * n, size=1).reshape(-1,1)
+            w = rng.multinomial(n, [1/n] * n, size=1).reshape(-1)
         elif method == 'gaussian':
             w = rng.standard_normal(size=n)
         else:
             raise ValueError("Invalid multiplier bootstrap method.")
         if function_class == "RKHS":
             score = score_rkhs(L, kwargs["K_sqrt"], w, seed=b)
-        elif function_class == "interval":
+        elif function_class == "intervals":
             score = score_intervals(
-                kwargs["X"], 
-                L, 
+                kwargs["X"].flatten(), 
+                L.flatten(), 
                 kwargs["threshold"], 
                 kwargs["epsilon"], 
                 w, 

src/fairness-audit/groups.py
@@ -30,24 +30,19 @@ def get_intersections(
     if depth == None:
         depth = X.shape[1]
     # all_groups = []
-    group_indices = []
+    group_dummies = []
     for n_intersect in range(1, depth + 1):
         for c in combinations(feature_list, n_intersect):
             unique_groups, indices = np.unique(X[:,c], return_inverse=True, axis=0)
 
-            # track groups
-            # all_groups.extend(
-            #     [{c_ind : g_val for c_ind, g_val in zip(c, g)} for g in unique_groups]
-            # )
-
             # generate dummies
             dummies = np.zeros((X.shape[0], len(unique_groups)), dtype=int)
             dummies[(range(X.shape[0]), indices)] = int(1)
-            group_indices.append(dummies)
+            group_dummies.append(dummies)
     
-    group_indices = np.concatenate(group_indices, axis=1, dtype=int)
+    group_dummies = np.concatenate(group_dummies, axis=1, dtype=int)
 
-    return group_indices
+    return group_dummies.astype(bool)
 
 def get_rectangles(
     X : np.ndarray,
@@ -108,7 +103,12 @@ def get_rectangles(
     group_dummies = np.einsum(einsum_str, *coordinate_dummies)
     group_dummies = group_dummies.reshape(n, -1, order='C') # flatten into (n, n_rectangles)
 
-    return group_dummies
+    # filter out duplicate groups
+    group_dummies = np.unique(group_dummies, axis=1)
+    if np.all(group_dummies[:,0] == 0):
+        group_dummies = group_dummies[:,1:]
+
+    return group_dummies.astype(bool)
 
 def score_intervals(
     X : np.ndarray,
@@ -123,6 +123,7 @@ def score_intervals(
     L = L[ind]
     w = w[ind]
 
+    # TODO: wrong...should be using bootstrap threshold
     if type == "lower":
         arr = -1 * (w - 1) * (L - threshold - epsilon)
         score = _max_subarray(arr)
@@ -143,7 +144,7 @@ def score_intervals(
 def _max_subarray(numbers):
     best_sum = 0  # or: float('-inf')
     current_sum = 0
-    for x in numbers:
+    for i, x in enumerate(numbers):
         current_sum = max(0, current_sum + x)
         best_sum = max(best_sum, current_sum)
     return best_sum

src/fairness-audit/metrics.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-from groups import Groups
-from typing import List, Union, Callable
+from typing import Callable
+
+def zero(Z, Y):
+    return 0
 
 def mean_predict(Z, Y):
     return np.mean(Z)
@@ -11,6 +13,11 @@ def mean_positive(Z, Y):
 
 def mean_negative(Z, Y):
     return np.mean(np.isclose(Z[np.isclose(Y, 0)], 1))
+
+def mean_coverage(Z, Y):
+    upper_cover = Z[:,1] >= Y.flatten()
+    lower_cover = Z[:,0] <= Y.flatten()
+    return np.mean(upper_cover & lower_cover)
 
 def error_rate(Z, Y):
     return ~np.isclose(Z, Y)
@@ -24,16 +31,23 @@ def equal_opportunity(Z, Y):
 def statistical_parity(Z, Y):
     return np.isclose(Z, 1)
 
+def equalized_coverage(Z, Y):
+    upper_cover = Z[:,1] >= Y.flatten()
+    lower_cover = Z[:,0] <= Y.flatten()
+    return (upper_cover & lower_cover).astype(int)
+
 _METRICS = dict(
     predictive_equality=predictive_equality,
     equal_opportunity=equal_opportunity,
-    statistical_parity=statistical_parity
+    statistical_parity=statistical_parity,
+    equalized_coverage=equalized_coverage
 )
 
 _THRESHOLDS = dict(
     predictive_equality=mean_negative,
     equal_opportunity=mean_positive,
-    statistical_parity=mean_predict
+    statistical_parity=mean_predict,
+    equalized_coverage=zero
 )
 
 class Metric:
@@ -79,9 +93,6 @@ class Metric:
         Z : np.ndarray = None,
         Y : np.ndarray = None
     ) -> np.ndarray:
-        # group_list = groups.definitions
-        # group_dummies = groups.dummies
-
         if 'calibration_bins' in self.metric_params:
             Z_disc = np.digitize(
                 Z,
@@ -89,10 +100,9 @@ class Metric:
             )
 
             z_vals, z_indices = np.unique(Z_disc, return_inverse=True)
-            z_dummies = np.zeros((Z.shape[0], len(z_vals)), dtype=int)
-            z_dummies[(range(Z.shape[0]), z_indices)] = int(1)
+            z_dummies = np.zeros((Z.shape[0], len(z_vals)), dtype=bool)
+            z_dummies[(range(Z.shape[0]), z_indices)] = True
 
-            # group_list = [[dict(grp, Z=z_val) for grp in group_list] for z_val in z_vals]
             group_dummies = np.einsum('ij,ik->kij', group_dummies, z_dummies)
 
         if 'y_values' in self.metric_params:
@@ -100,16 +110,8 @@ class Metric:
             y_dummies = np.isclose(Y, y_vals)
 
             if group_dummies.ndim > 2:
-                # group_list = [
-                #     [
-                #         dict(grp, Y=y_val) for grp in g_list
-                #     ]
-                #     for g_list in group_list
-                #     for y_val in y_vals
-                # ]
                 group_dummies = np.concatenate(np.einsum('kij,il->klij', group_dummies, y_dummies), axis=0)
             else:
-                # group_list = [[dict(grp, Y=y_val) for grp in group_list] for y_val in y_vals]
                 group_dummies = np.einsum('ij,ik->kij', group_dummies, y_dummies)
 
         return group_dummies