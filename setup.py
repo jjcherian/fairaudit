1ccff06227ebb28a2814f697df0fed3a6d7cce20
John Cherian <john.jcheri@gmail.com>
2023-03-28 13:08:12 -0700

    added requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import subprocess
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+   long_description = fh.read()
+
+# get all the git tags from the cmd line that follow our versioning pattern
+git_tags = subprocess.Popen(
+    ["git", "tag", "--list", "v*[0-9]", "--sort=version:refname"],
+    stdout=subprocess.PIPE,
+)
+tags = git_tags.stdout.read()
+git_tags.stdout.close()
+tags = tags.decode("utf-8").split("\n")
+tags.sort()
+
+# PEP 440 won't accept the v in front, so here we remove it, strip the new line and decode the byte stream
+VERSION_FROM_GIT_TAG = tags[-1][1:]
+
+setup(
+    name="fairaudit",
+    version=VERSION_FROM_GIT_TAG,  # Required
+    setup_requires=["setuptools>=18.0"],
+    packages=find_packages(exclude=["notebooks"]),  # Required
+    install_requires=[
+        "numpy",
+        "scipy",
+        "cvxpy",
+	"sklearn",
+	"matplotlib",
+	"statsmodels",
+	"tqdm"
+    ],
+    description="This package enables fairness auditing with statistical guarantees.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jjcherian/fairaudit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    author="John Cherian",
+    author_email="jcherian@stanford.edu",
+)
